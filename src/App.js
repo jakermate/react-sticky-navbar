57c942e95cf66725b277b787b5da9125af097b0e
jm <root1@root.local>
2020-09-28 10:23:17 -0700

    Added params object to parse props into valid values or defaults.  Added proptypes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './navbar/index'
+import brand from './logo.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar locations={[]} brandIcon={brand}></Navbar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/navbar/index.js
+++ src/navbar/index.js
@@ -1,10 +1,78 @@
-import React from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import proptypes from 'prop-types'
 import styled from 'styled-components'
-export default function index(props) {
+export default function Index(props) {
+    const log = console.log
+    // setup defaults if none provided
+    const params = {
+        locations: props?.locations || [],
+        brandIcon : processBrandImage(props?.brandIcon) // image element
+    }
+
+    // location tracking
+    const [active, setActive] = useState('/')
+    const [map, setMap] = useState({})
+    const [windowHeight, setWindowHeight] = useState(0)
+    // setup listeners
+    useEffect(()=>{
+        document.addEventListener('scroll', onscroll)
+        document.addEventListener('resize', resizeWindow)
+        buildMap(params.locations)
+        resizeWindow()
+    },[]) // on mount
+    function onscroll(){
+        let position = getScrollLocation()
+        log(position)
+        determineIfActive(position)
+    }
+    function getScrollLocation(){
+        return window.scrollY
+    }
+    function buildMap(pathArray){
+        let map = []
+        pathArray.forEach((pathObj, index)=>{
+            let newObj = {
+                path: pathObj.path,
+                location: getLocation(pathObj.path)
+            }
+            map.push(newObj)
+        })
+        return map
+    }
+    function getLocation(path){
+        let el = document.getElementById(path)
+        return el.getBoundingClientRect().top + document.body.scrollTop
+    }
+    function resizeWindow(){
+        let windowHeight = getWindowSize()
+        setWindowHeight(windowHeight)
+    }
+    function getWindowSize(){
+        return window.innerHeight
+    }
+    // apply active class
+    const headerRef = useRef()
+    function determineIfActive(){
+        if(window.scrollY > 10){
+            headerRef.current.classList.add('active')
+            return
+        }
+        headerRef.current.classList.remove('active')
+    }
     return (
-        <Header className="">
-            
+        <Header ref={headerRef} className="" style={{
+            position:'sticky',
+            top: 0,
+            left: 0,
+            right: 0,
+            width: '100%',
+            background:'white'
+        }}>
+            <div id="react-sticky-header-brand-container">
+                
+                {params.brandIcon}
+                
+            </div>
         </Header>
     )
 }
@@ -13,8 +81,39 @@ const Header = styled.header`
     top:0;
     left:0;
     right:0;
-    min-height: 70px;
+    min-height: 50px;
+    padding: 0 20px;
+    transition: all .3s cubic-bezier(0.68, -0.6, 0.32, 1.6); 
     &.active{
-
+        background: black !important;
+        padding: 0 10px;
     }
 `
+
+Index.propTypes = {
+    /** type:[object] - object array of element ID to include in navigation section 
+     * {
+     *     path: string,
+     *     image: string
+     * }
+    */
+    locations: proptypes.object,
+    /** type:string - string to path of image*/
+    brandIcon: proptypes.string
+}
+
+function processBrandImage(brandImage){
+    let img = new Image()
+    img.src = brandImage
+    let width = img.width
+    let height = img.height
+    console.log(img)
+    if(width > height){
+        img.width = 30
+        return img
+    }
+    else{
+        img.width = 30
+        return img
+    }
+}
